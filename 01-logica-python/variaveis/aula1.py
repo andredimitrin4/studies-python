df2a26b73ede959d94a85d1c9bf1839c96609060
André Dimitrin <andredimitrin4@gmail.com>
2025-12-17 21:59:45 -0300

    Atualização das aulas

--- 01-logica-python/variaveis/aula1.py
+++ 01-logica-python/variaveis/aula1.py
@@ -1,2 +1,16 @@
-print('Hello world!')
-print('Opa,editei.')
+"""
+DocString
+E escrever o que eu 
+quiser
+
+"""
+'''
+Usar para escrever suas notas
+
+'''
+#
+#Permite escrever um comentário
+print("Olá, Mundo!")  #Exibe a mensagem na tela
+#Fim do programa
+print(123)  #Exibe o número 123 na tela
+#
---
+++ 01-logica-python/variaveis/aula3.py
@@ -0,0 +1,21 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica/Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+
+"""
+
+print(1234)
+
+#Aspas simples
+print('"André Dimitrin"')
+
+#Aspas duplas
+print("André Dimitrin")
+
+#Escape
+print('André Dimitrin')
+
+#r
+print(r"André \"Dimitrin\"")
